4d6783f50a9fc6e9c35a5f06aec6ae0d94c54979
Nikola Milojevic <nmilojevic@users.noreply.github.com>
2017-09-04 16:05:54 +0200

    Update persevering_transaction.rb
    Fix deprecate   alias_method_chain

--- lib/acts_as_ordered_tree/persevering_transaction.rb
+++ lib/acts_as_ordered_tree/persevering_transaction.rb
@@ -70,12 +70,12 @@ module ActsAsOrderedTree
 
     # Marks this transaction as committed and executes its commit callbacks
     # @api private
-    def committed_with_callbacks!
-      committed_without_callbacks!
+    def committed!
+      super
       @callbacks.each { |callback| callback.call }
     end
-    alias_method_chain :committed!, :callbacks
-
+    self.class.prepend(ActsAsOrderedTree::PerseveringTransaction)
+    
     private
     def pause
       sleep(rand(attempts) * 0.1)
